0a3eec3209ce06da2475f6409ddbdabd9ce95874
holdenhughes <holdenmh121489@gmail.com>
2015-11-04 11:52:52 -0500

    Initial commit

---
+++ src/com/company/Event.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by holdenhughes on 11/4/15.
+ */
+public class Event {
+    int id;
+    String description;
+    LocalDateTime startDate;
+}
---
+++ src/test/com/company/MainTest.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by holdenhughes on 11/4/15.
+ */
+public class MainTest {
+    public Connection startConnection() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:./test");
+        Main.createTable(conn);
+        return conn;
+    }
+
+    public void endConnection(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("DROP TABLE events");
+        conn.close();
+    }
+
+    @Test
+    public void testEvents() throws SQLException {
+        Connection conn = startConnection();
+        Main.insertEvent(conn, "This is a test", LocalDateTime.now());
+        ArrayList<Event> events = Main.selectEvents(conn);
+        endConnection(conn);
+
+        assertTrue(events.size() ==1);
+    }
+
+}
